f6a83c47ab66963428c2f5dd8d98bd7cdb9fa72c
pranaylobo <lobopranayk9@gmail.com>
2020-05-12 12:38:23 +0530

    second

--- server.js
+++ server.js
@@ -1,15 +1,42 @@
 'use strict';
+const express = require('express')
 
 var os = require('os');
 var nodeStatic = require('node-static');
 var http = require('http');
 var socketIO = require('socket.io');
+const PORT = process.env.PORT || 3000;
+const app1 = express();
 
 var fileServer = new(nodeStatic.Server)();
+
+app1.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'https://webrtcconnect1.herokuapp.com');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
 var app = http.createServer(function(req, res) {
   fileServer.serve(req, res);
-}).listen(8084);
+    console.log("hello")
+
+}).listen(PORT);
   
+
+
 var io = socketIO.listen(app);
 io.sockets.on('connection', function(socket) {
 
